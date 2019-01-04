e0f9f773bbc5349ed1901f72dd906da39622613d
carolcesp <carolinacespedes90@gmail.com>
2019-01-04 11:38:08 +0100

    add html and css to app.js from wildcoders

--- reactivegirls/src/App.js
+++ reactivegirls/src/App.js
@@ -1,25 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './images/logo-card.svg';
+import logoAdalab from './images/logo-adalab.png';
 import './App.css';
 
 class App extends Component {
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+      <div className="page">
+        <div className="home__container">
+          <div className="page__wrapper">
+            <header className="home__logo">
+            <img src={logo} className="App-logo" alt="logo" />
+            </header>
+            <main className="home__main">
+              <h1 className="home__main--title">Crea tu tarjeta de visita</h1>
+
+              <p className="home__main--text">Crea mejores contactos profesionales de forma fácil y cómoda</p>
+              <ul className="home__options">
+                <li className="home__options--item"> <i className="home__options--icon far fa-object-ungroup"></i>
+
+                  <div className="home__options--text">Diseña</div>
+                </li>
+                <li className="home__options--item"> <i className="home__options--icon far fa-keyboard"></i>
+
+                  <div className="home__options--text">Rellena</div>
+                </li>
+                <li className="home__options--item"> <i className="home__options--icon fas fa-share-alt"></i>
+
+                  <div className="home__options--text">Comparte</div>
+                </li>
+              </ul> <a href="card_editor.html">
+                <div className="home__link">Comenzar</div>
+              </a>
+
+            </main>
+          </div>
+        </div>
+        <footer className="footer">
+          <div className="footer__copyright">Awesome profile-cards @2018</div> <a href="https://adalab.es/">
+            <div className="footer__logo">
+            <img src={logoAdalab} className="footer__logo-img" alt="logo" />
+            </div>
           </a>
-        </header>
+
+        </footer>
       </div>
     );
   }
