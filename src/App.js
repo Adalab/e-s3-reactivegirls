934e8984ec7ff5770688b2e89ad6225ba06d777b
Silvia García Cacho <silvia@garca.info>
2019-01-15 11:22:44 +0100

    Insert spread method

--- src/App.js
+++ src/App.js
@@ -9,31 +9,36 @@ class App extends Component {
   constructor (props) {
     super(props);
     this.state = {
-      name: "Nombre y apellidos",
-      job: "Front end developer",
-      email: "",
-      phone: "",
-      img: "",
-      linkedin: "",
-      github:""
+      card:{
+        name: "Nombre y apellidos",
+        job: "Front end developer",
+        email: "",
+        phone: "",
+        img: "",
+        linkedin: "",
+        github:""
+      }
+      
     };
      this.handleKeyUpN = this.handleKeyUpN.bind(this);
      this.handleKeyUpJ = this.handleKeyUpJ.bind(this);
-
+     
  }
-
+ 
   handleKeyUpN(event) {
-     this.setState({ name: event.currentTarget.value });
+    const cardO = this.state.card
+     this.setState({ card: {...cardO, name: event.currentTarget.value} });
    }
   handleKeyUpJ(event) {
-    this.setState({ job: event.currentTarget.value }); 
+    const cardO = this.state.card
+    this.setState({ card: {...cardO, job: event.currentTarget.value} }); 
   }
 
   render() {
 
     return (
          
-     <Cards skills={skills} handleKeyUpN={this.handleKeyUpN} handleKeyUpJ={this.handleKeyUpJ} name={this.state.name} job={this.state.job}/>
+     <Cards skills={skills} handleKeyUpN={this.handleKeyUpN} handleKeyUpJ={this.handleKeyUpJ} name={this.state.card.name} job={this.state.card.job}/>
      
     );
   }
